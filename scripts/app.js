4e04543eb2f70ac70c862fd03074e5b5cb401f32
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-02-06 15:50:12 -0800

    used ajax with google maps to show all earthquakes in the last week

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,48 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
+
 $(document).ready(function() {
+
   console.log("Let's get coding!");
   // CODE IN HERE!
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.78, lng: -122.44},
+    zoom: 8,
+  });
+
+  var SanFran = new google.maps.Marker({
+    position: {lat: 37.78, lng: -122.44},
+    map: map,
+    title: "SF"
+  })
+
+  $.ajax({
+  method: "GET",
+  url: weekly_quakes_endpoint,
+  success: function(response) {
+    console.log(response.features);
+    $(response.features).each(function() {
+      var title = this.properties.title;
+      var timeSince = ((Date.now() - this.properties.time) / 3600000).toFixed(1);
+      var quakeMarker = new google.maps.Marker({
+        position: {lat: this.geometry.coordinates[1], lng: this.geometry.coordinates[0]},
+        map: map,
+        title: title
+      });
+
+      console.log(timeSince);
+      $("#info").html($("#info").html() + "<p>Title: " + title + " / " + timeSince + " hours ago");
+    })
+  },
+  error: function() {
+      alert("There was an error getting data.");
+  }
+ });
+
+  $ajax({
+
+  })
 
 });
